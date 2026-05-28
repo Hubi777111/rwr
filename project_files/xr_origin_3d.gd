08e89eaf0044a5e132ffac5539da7cc8d5c089e0
Hubert Wiśniewski <hubert.biuro512@gmail.com>
2026-05-28 22:36:51 +0200

    Zadanie 5: Dodanie płynnej lokomocji na lewej gałce

--- project_files/xr_origin_3d.gd
+++ project_files/xr_origin_3d.gd
@@ -25,7 +25,8 @@ func _physics_process(delta: float) -> void:
 	if v.length() < deadzone:
 		v = Vector2.ZERO
 
-	dir += fwd * (-v.y) + right * (v.x)
+	# POPRAWKA: Usunięto minus przed v.y
+	dir += fwd * (v.y) + right * (v.x)
 
 	if dir.length() > 0.0:
 		global_translate(dir.normalized() * move_speed * delta)
